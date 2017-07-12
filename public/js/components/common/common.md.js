f3144f331774dd4ffe3bc839ac6e3a3726b364db
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-07-12 12:09:16 +0200

    removed the require for moment in the common cuz it raises errors

--- public/js/components/common/common.md.js
+++ public/js/components/common/common.md.js
@@ -1,8 +1,6 @@
 import navbarComponent from './navbar'
 import * as moment from 'moment';
 import 'moment/locale/fr';
-var moment = require('moment');
-moment().format();
 
 let commonModule = angular.module('app.common', [])
     .component('navbar', navbarComponent)
