a48109f8ebe11c516adaca4c429edbbcbc79fad7
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-07-12 12:01:48 +0200

    add the require moment in common.md.js

--- public/js/components/common/common.md.js
+++ public/js/components/common/common.md.js
@@ -1,6 +1,8 @@
 import navbarComponent from './navbar'
 import * as moment from 'moment';
 import 'moment/locale/fr';
+var moment = require('moment');
+moment().format();
 
 let commonModule = angular.module('app.common', [])
     .component('navbar', navbarComponent)
